ab627a8e4258c99cb933b89bc66274d44502e750
Brigido Alejo <brigido.alejo@consumercloud.co.uk>
2016-08-19 14:49:27 +0100

    added BaseUrl constants

---
+++ src/constant/BaseUrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by brigido.alejo on 8/19/2016.
+ */
+'use strict';
+
+export const GITHUB = 'https://github.com';
+export const GITHUB_PROJECTS_VR = 'https://github.com/showcases/virtual-reality';
