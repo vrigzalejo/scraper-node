84486cb4d9317703bf9e671913cc6a75ca5e8d1a
Brigido Alejo <brigido.alejo@consumercloud.co.uk>
2016-08-19 15:24:55 +0100

    github controller's saving to mongodb and exporting to csv/tsv works

--- src/controller/Github.js
+++ src/controller/Github.js
@@ -5,47 +5,82 @@
 'use strict';
 
 import dependencies from '../dependencies'
+import GithubModel from '../model/GithubModel'
+import * as FileExtension from '../constant/FileExtension'
+import * as BaseUrl from '../constant/BaseUrl'
 
 class Github {
 
   static scrape(req, res) {
-    let url = "https://github.com/showcases/virtual-reality";
+
+    let url = BaseUrl.GITHUB_PROJECTS_VR;
+
+    let gitModel = GithubModel.mongoModel();
 
     dependencies.Request(url, (error, response, html) => {
-      
+
+      let created = dependencies.Moment().format('MM-DD-YYYY-hh-mm-ssA');
+      let file = 'github-' + created + FileExtension.CSV;
+
       if(!error) {
         let $ = dependencies.Cheerio.load(html);
 
-        let title, release, rating;
-        let json = {title: "", release: "", rating: ""};
+        let repoLists = $('ul.repo-list > li.repo-list-item');
 
-        $('.header').filter(() => {
-          let data = $(this);
+        repoLists.each((i, elem) => {
 
-          title = data.children().first().text();
+          let _data = {
+            url : BaseUrl.GITHUB + $(elem).find('h3 > a').attr('href'),
+            repoName : $(elem).find('a').text().replace(/[\s\n]/g, ''),
+            description : $(elem).find('.repo-list-description').text().trim(),
+          };
 
-          release = data.children().last().children().text();
+          let getDNSandSave = (function(DNS, url, file, _data, created, delimeter) {
+              return (err, addresses) => {
+                if (err) throw err;
 
-          json.title = title;
-          json.release = release;
+                const model = new GithubModel(_data, addresses, BaseUrl.GITHUB_PROJECTS_VR, created);
+
+                let dlmtr = ',';
+                switch(delimeter) {
+                  case FileExtension.TSV:
+                    dlmtr = "\t";
+                }
+
+                let newData = [
+                  model.data.url, model.data.repoName, model.data.description,
+                  model.ipAddress.join(';'), model.url,model.created
+                ];
+
+                let mongoData = new gitModel({
+                  data: dependencies.Serialize.serialize(model.data),
+                  ip_address: model.ipAddress.join(';'),
+                  url: BaseUrl.GITHUB_PROJECTS_VR,
+                  created: created,
+                });
+
+                mongoData.save(function (err) {
+                  if (err) console.log(err);
+                });
+
+                if(!dependencies.FS.existsSync(dependencies.config.logs.path)) {
+                  dependencies.FS.mkdirSync(dependencies.config.logs.path);
+                }
+                dependencies.FS.appendFile(dependencies.config.logs.path + dependencies.Path.sep + file, '"' + newData.join('"' + dlmtr + '"') + '"\n', (err) => {
+                  console.log(`File successfully written! - Check ${file}`);
+                });
+
+                console.log(newData);
+
+              };
+          }(dependencies.DNS, BaseUrl.GITHUB.replace(/^https?:\/\//, ''), file, _data, created, FileExtension.CSV));
+
+          dependencies.DNS.resolve4(BaseUrl.GITHUB.replace(/^https?:\/\//, ''), getDNSandSave);
 
         });
 
-
-        $('.star-box-giga-star').filter(() => {
-          let data = $(this);
-
-          rating = data.text();
-
-          json.rating = rating;
-        });
-
-        dependencies.FS.writeFile('output.json', JSON.stringify(json, null, 4), (err) => {
-          console.log('File successfully written! - Check your project directory for the output.json file');
-        });
-
-        res.send('Check your console!');
       }
+      res.send(`Check the log "${file}" in your project folder =)`);
     });
   }
 }
