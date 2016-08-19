9837da5d23d8deef9343b261423f07abf50bc3b0
Brigido Alejo <brigido.alejo@consumercloud.co.uk>
2016-08-19 14:53:08 +0100

    updated dependencies

--- src/dependencies.js
+++ src/dependencies.js
@@ -9,19 +9,41 @@ import Moment from 'moment'
 import Request from 'request'
 import Cheerio from 'cheerio'
 import Mongoose from 'mongoose'
+import Serialize from 'node-serialize'
+import DNS from 'dns'
+// config modules
 import config from '../config.json'
+// constants
+import FileExtension from './constant/FileExtension'
+// controllers
+import Github from './controller/Github'
+// database
+import Mongo from './db/Mongo'
+
 
 let dependencies = {
     // Node modules
     Express: Express,
+    DNS: DNS,
     FS: FS,
     Moment: Moment,
     Request: Request,
     Cheerio: Cheerio,
     Mongoose: Mongoose,
+    Serialize: Serialize,
 
-    // Custom modules
-    config: config
+    // config modules
+    config: config,
+
+    // constants
+    FileExtension: FileExtension,
+
+    // controllers
+    Github: Github,
+
+    // database
+    Mongo: Mongo
+
 };
 
 export default dependencies;
